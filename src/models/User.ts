54474bd959e613f7e2fccf4cb1a1cc2019213aa3
Balaji Asish <balajiashish02@gmail.com>
2025-08-13 14:55:22 +0530

    login and signup

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,20 @@
 import { Schema, model } from "mongoose";
 
-const carSchema = new Schema({
-  make: { type: String, required: true, },
-  model: { type: String, required: true},
+const carSchema: Schema = new Schema({
+  make: { type: String, required: true },
+  model: { type: String, required: true },
   year: { type: Number, required: true },
-  color: { type: String, required: true,},
+  color: { type: String, required: true },
   price: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
-export const CarModel = model("Car", carSchema);
+const UserSchema: Schema = new Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
+export const CarModel = model("Car", carSchema);
+export const userModel = model("user",UserSchema);
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,77 @@ import express from "express";
 import type { Request, Response } from "express";
 import { connectDB } from "./config/db.js";
 import { CarModel } from "./models/User.js";
+import jwt, { type Secret } from "jsonwebtoken";
+import dotenv from "dotenv";
+import { userModel } from "./models/User.js";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 await connectDB();
+
+app.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const details = req.body;
+
+    if (!details) {
+      return res.status(400).json({ message: "Please provide details" });
+    }
+
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      return res.status(500).json({ message: "JWT secret is not defined" });
+    }
+
+    const checking = await userModel.findOne({
+      email: details.email,
+    });
+
+    if (checking) {
+      return res.json({
+        message: "user already exists",
+      });
+    }
+
+    const user = await userModel.create(details);
+
+    const payload = { email: user.email, id: user._id };
+    const token = jwt.sign(payload, secret);
+
+    res.json({ token });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
+
+app.post("/login", async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.token as string | undefined;
+
+    if (!token) {
+      return res.status(401).json({ message: "You are not authenticated." });
+    }
+
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      return res.status(500).json({ message: "JWT secret is not defined." });
+    }
+
+    const decoded = jwt.verify(token, secret as Secret) as { email: string };
+
+    const userdetails = await userModel.findOne({ email: decoded.email });
+    if (!userdetails) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    res.status(200).json({ message: "User authenticated.", userdetails });
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token.", error: err });
+  }
+});
 
 app.post("/cars", async (req: Request, res: Response) => {
   try {
@@ -20,15 +86,28 @@ app.post("/cars", async (req: Request, res: Response) => {
   }
 });
 
-app.get("/cars" , async(req:Request,res:Response)=>{
-  try{
+app.get("/cars", async (req: Request, res: Response) => {
+  try {
     const response = await CarModel.find();
     res.json(response);
+  } catch (e: any) {
+    console.error("error while fetching the data" + e.message);
   }
-  catch(e:any){
-    console.error("error while fetching the data" + e.message)
+});
+
+app.post("/carname", async (req: Request, res: Response) => {
+  try {
+    const body = req.body.carname;
+    const response = await CarModel.find({
+      model: body,
+    });
+    res.json({
+      response,
+    });
+  } catch (e: any) {
+    console.error("error");
   }
-})
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
