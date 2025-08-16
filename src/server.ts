452211cedb1274787107f500f1a100a971608b3e
Balaji Asish <balajiashish02@gmail.com>
2025-08-16 22:04:35 +0530

    fixes 2

--- src/server.ts
+++ src/server.ts
@@ -153,7 +153,6 @@ app.post(
       const color = req.body.color;
       const response = await CarModel.find({
         color: color,
-        userId: req.user._id,
       });
       res.json(response);
     } catch (e) {
